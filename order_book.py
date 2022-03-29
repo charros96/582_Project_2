aeb31bf917998088fcf78e75683bd20773f62e2e
charros96 <88594194+charros96@users.noreply.github.com>
2022-03-28 17:20:23 -0700

    Add files via upload

--- order_book.py
+++ order_book.py
@@ -12,6 +12,18 @@ def process_order(order):
     fields = ['sender_pk','receiver_pk','buy_currency','sell_currency','buy_amount','sell_amount']
     order_obj = Order(**{f:order[f] for f in fields})
 
+    
+    unfilled_db = session.query(Order).filter(Order.filled == "").all()
+    for existing_order in unfilled_db:
+        if existing_order.buy_currency == order.sell_currency:
+            if existing_order.sell_currency == order.buy_currency:
+                if existing_order.sell_amount / existing_order.buy_amount >= order.buy_amount/order.sell_amount :
+                    existing_order.filled = datetime.timestamp()
+                    order_obj.filled = datetime.timestamp()
+                    existing_order.counterparty_id = order_obj.id
+                    order_obj.counterparty_id = existing_order.id
+                    break
+
     session.add(order_obj)
     session.commit()
     pass
