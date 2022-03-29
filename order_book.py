f9f8db7398d225c2de18cc0df424098dc7a4cadc
charros96 <88594194+charros96@users.noreply.github.com>
2022-03-28 17:22:48 -0700

    Add files via upload

--- order_book.py
+++ order_book.py
@@ -18,8 +18,9 @@ def process_order(order):
         if existing_order.buy_currency == order.sell_currency:
             if existing_order.sell_currency == order.buy_currency:
                 if existing_order.sell_amount / existing_order.buy_amount >= order.buy_amount/order.sell_amount :
-                    existing_order.filled = datetime.timestamp()
-                    order_obj.filled = datetime.timestamp()
+                    ct = datetime.datetime.now()
+                    existing_order.filled = ct
+                    order_obj.filled = ct
                     existing_order.counterparty_id = order_obj.id
                     order_obj.counterparty_id = existing_order.id
                     break
